f39439a5f99b21bdaf2b0ca495b9449dd1dd07a9
Bunleap <bunleap.sorn@gmail.com>
2024-03-06 22:00:09 +0700

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,11 +1,22 @@
 import java.util.Stack;
+import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Palindrome {
     public static void main(String[] args) {
-        System.out.println(isPalindrome("racecar")); // true
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        isPalindromeStack(input);
+        isPalindromeQueue(input);
+
+        scanner.close();
     }
 
-    public static boolean isPalindrome(String s) {
+    public static boolean isPalindromeStack(String s) {
         Stack<Character> stack = new Stack<Character>();
         String reverse = "";
 
@@ -19,7 +30,43 @@ public class Palindrome {
             reverse += stack.pop();
         }
 
+        System.out.println("\nMODE\t: Stack");
+
         // Check if the strings are equal
-        return s.equals(reverse);
+        if (s.equals(reverse)) {
+            System.out.printf("Input\t: %s\nReverse\t: %s \nOutput\t: THAT IS A PALINDROME!\n", s, reverse);
+            return true;
+        } else {
+            System.out.printf("Input\t: %s\nReverse\t: %s \nOutput\t: THAT IS NOT A PALINDROME!\n", s, reverse);
+            return false;
+        }
+
+    }
+
+    public static boolean isPalindromeQueue(String s) {
+        Queue<Character> queue = new LinkedList<Character>();
+        String reverse = "";
+
+        // Queue all characters
+        for (int i = s.length() - 1; i >= 0; i--) {
+            queue.add(s.charAt(i));
+        }
+
+        // Reverse the string
+        while (!queue.isEmpty()) {
+            reverse += queue.remove();
+        }
+
+        System.out.println("\nMODE\t: Queue");
+
+        // Check if the strings are equal
+        if (s.equals(reverse)) {
+            System.out.printf("Input\t: %s\nReverse\t: %s \nOutput\t: THAT IS A PALINDROME!\n", s, reverse);
+            return true;
+        } else {
+            System.out.printf("Input\t: %s\nReverse\t: %s \nOutput\t: THAT IS NOT A PALINDROME!\n", s, reverse);
+            return false;
+        }
+
     }
 }
